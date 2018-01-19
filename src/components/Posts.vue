88fae83cf49074f1737c4b7362cdfec22e7db697
calirojas <juancarlosr@msn.com>
2018-01-18 19:35:45 -0600

    Adding pug, scss, axios and bootstrap. Template cleaning (demo code added by vue-cli was removed). Two new components added for posts and gallery.

---
+++ src/components/Posts.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  .c-posts
+    .row
+      .col-sm-12
+        h1 reddit posts
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+  .c-posts{
+
+  }
+</style>
