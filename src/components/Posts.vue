72fae1d9e642efd01c15e865a8c9ee2fcb3a8693
calirojas <juancarlosr@msn.com>
2018-01-18 20:03:52 -0600

    Vuex added. Empty store configuration

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -2,11 +2,35 @@
   .c-posts
     .row
       .col-sm-12
-        h1 reddit posts
+        h1 Posts
+    .row(
+      v-if="posts.length"
+    )
+      .col-sm-2
+        ul.list-group
+          li.list-group-item(
+            v-for="post in posts"
+          ) {{post.title}}
+      .col-sm-10
+    .row(
+      v-else
+    )
+      .col-sm-12
+        .alert.alert-info.small No posts to show
 </template>
 <script>
-export default {
+import { mapGetters } from 'vuex'
 
+export default {
+  data () {
+    return {
+      posts: []
+    }
+  },
+  computed: {
+    ...mapGetters([
+    ])
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
 import router from './router'
+import store from './store'
 
+Vue.use(Vuex)
 Vue.config.productionTip = false
 
 require('bootstrap/dist/css/bootstrap.min.css')
@@ -12,6 +15,7 @@ require('bootstrap/dist/css/bootstrap.min.css')
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
